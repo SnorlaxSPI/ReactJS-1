4bfdfacf0220ccd13e348e6d36c58cf1ddff6db3
aflinsjr@icloud.com <aflinsjr@icloud.com>
2023-04-08 15:00:24 -0300

    03 - Estado do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { RepositoryList } from './components/RepositoryList.jsx';
+import { Counter } from './components/Counter.jsx';
 
 import React from 'react';
 
 import './styles/global.scss';
 
 export function App() {
-  return <RepositoryList />
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  )
 };
